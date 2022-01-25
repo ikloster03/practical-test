02ed97882695755f85ac41273403d684d958c571
Ivan Monastyrev <ikloster@yandex.ru>
2022-01-25 23:48:50 +0300

    server(client): update model

--- server/src/modules/client/client.model.js
+++ server/src/modules/client/client.model.js
@@ -1,26 +1,75 @@
 const mongoose = require('mongoose');
+const yup = require('yup');
 
-const ClientSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+require('yup-phone');
+
+const emailSchema = yup.string().email().required();
+const phoneSchema = yup.string().phone('US').required();
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Client:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *        name:
+ *          type: string
+ *        email:
+ *          type: string
+ *          format: email
+ *        phone:
+ *          type: string
+ *        providers:
+ *          type: array
+ *        seeded:
+ *          type: boolean
+ *        example:
+ *          id: 5ebac534954b54139806c112
+ *          name: fake name
+ *          email: fake@example.com
+ *          phone: 2024561414
+ *          providers: []
+ *          seeded: false
+ */
+const ClientSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => emailSchema.validateSync(v),
+        message: (props) => `${props.value} is not a valid email address!`,
+      },
+    },
+    phone: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => phoneSchema.validateSync(v),
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+    },
+    providers: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'Provider',
+      },
+    ],
+    seeded: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  phone: {
-    type: String,
-    required: true,
-    // match: [
-    //   /^[1-9][0-9]{9}$/,
-    //   'The value of path {PATH} ({VALUE}) is not a valid mobile number.',
-    // ],
-  },
-  providers: {
-    type: String,
-    required: true,
-  },
-});
+);
 
 module.exports = mongoose.model('Client', ClientSchema);
