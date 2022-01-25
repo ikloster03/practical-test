466dd8280d1494b73f6276696fc55a4fff91da8b
Ivan Monastyrev <ikloster@yandex.ru>
2022-01-25 23:50:51 +0300

    server(provider): update controller

--- server/src/modules/provider/provider.controller.js
+++ server/src/modules/provider/provider.controller.js
@@ -1,18 +1,35 @@
-const create = (req, res, next) => {
-  res.json({ test: true });
-};
-const list = (req, res, next) => {
-  res.json({ test: true });
-};
-const one = (req, res, next) => {
-  res.json({ test: true });
-};
-const update = (req, res, next) => {
-  res.json({ test: true });
-};
-const remove = (req, res, next) => {
-  res.json({ test: true });
-};
+const { wrapAsync } = require('../../utils');
+const providerService = require('./provider.service');
+
+const create = wrapAsync(async (req, res) => {
+  const provider = await providerService.create(req.body);
+
+  res.json(provider);
+});
+
+const list = wrapAsync(async (req, res) => {
+  const providers = await providerService.list();
+
+  res.json(providers);
+});
+
+const one = wrapAsync(async (req, res) => {
+  const provider = await providerService.one(req.params.id);
+
+  res.json(provider);
+});
+
+const update = wrapAsync(async (req, res) => {
+  const provider = await providerService.update(req.params.id, req.body);
+
+  res.json(provider);
+});
+
+const remove = wrapAsync(async (req, res) => {
+  const provider = await providerService.remove(req.params.id);
+
+  res.json(provider);
+});
 
 module.exports = {
   create, list, one, update, remove,
