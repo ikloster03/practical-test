e199fe22e99d5fa58b8b616ccd7dd4162a262f98
Ivan Monastyrev <ikloster@yandex.ru>
2022-01-25 23:42:29 +0300

    server: add seeding clients

---
+++ server/migrations/20220119194653-seeding-clients.js
@@ -0,0 +1,46 @@
+const faker = require('faker');
+
+const AMOUNT_OF_CLIENTS = 15;
+
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function* fakeClientGenerator() {
+  let i = 0;
+
+  while (i < AMOUNT_OF_CLIENTS) {
+    i += 1;
+    yield {
+      name: faker.fake('{{name.firstName}} {{name.lastName}}'),
+      email: faker.internet.email(),
+      phone: faker.phone.phoneNumber('##########'),
+      providers: [],
+      seeded: true,
+    };
+  }
+}
+
+async function getRandomProviders(dbProvider) {
+  return dbProvider.aggregate(
+    [{ $sample: { size: getRandom(0, 4) } }],
+  ).toArray();
+}
+
+module.exports = {
+  async up(db) {
+    const fakeClients = [...fakeClientGenerator()];
+
+    for await (const client of fakeClients) {
+      const fakeProviders = await getRandomProviders(db.collection('providers'));
+
+      client.providers = fakeProviders.map((p) => p._id);
+    }
+
+    await db.collection('clients').insertMany(fakeClients);
+  },
+
+  async down(db) {
+    await db.collection('clients').deleteMany({ seeded: true });
+  },
+};
