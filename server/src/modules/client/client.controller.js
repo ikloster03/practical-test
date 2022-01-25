aa52c476e271f3fa2d93920947e0e0f44873188d
Ivan Monastyrev <ikloster@yandex.ru>
2022-01-25 23:49:16 +0300

    server(client): update controllers

--- server/src/modules/client/client.controller.js
+++ server/src/modules/client/client.controller.js
@@ -1,18 +1,35 @@
-const create = (req, res, next) => {
-  res.json({ test: true });
-};
-const list = (req, res, next) => {
-  res.json({ test: true });
-};
-const one = (req, res, next) => {
-  res.json({ test: true });
-};
-const update = (req, res, next) => {
-  res.json({ test: true });
-};
-const remove = (req, res, next) => {
-  res.json({ test: true });
-};
+const { wrapAsync } = require('../../utils');
+const clientService = require('./client.service');
+
+const create = wrapAsync(async (req, res) => {
+  const client = await clientService.create(req.body);
+
+  res.json(client);
+});
+
+const list = wrapAsync(async (req, res) => {
+  const clients = await clientService.list();
+
+  res.json(clients);
+});
+
+const one = wrapAsync(async (req, res) => {
+  const client = await clientService.one(req.params.id);
+
+  res.json(client);
+});
+
+const update = wrapAsync(async (req, res) => {
+  const client = await clientService.update(req.params.id, req.body);
+
+  res.json(client);
+});
+
+const remove = wrapAsync(async (req, res) => {
+  const client = await clientService.remove(req.params.id);
+
+  res.json(client);
+});
 
 module.exports = {
   create, list, one, update, remove,
