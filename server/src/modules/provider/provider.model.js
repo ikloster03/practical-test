d956bddff2efd2275180218ddb9301edc6302ca6
Ivan Monastyrev <ikloster@yandex.ru>
2022-01-25 23:49:48 +0300

    server(provider): update model

--- server/src/modules/provider/provider.model.js
+++ server/src/modules/provider/provider.model.js
@@ -1,10 +1,37 @@
 const mongoose = require('mongoose');
 
-const ProviderSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Provider:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *        name:
+ *          type: string
+ *        seeded:
+ *          type: boolean
+ *        example:
+ *          id: 5ebac534954b54139806c112
+ *          name: fake name
+ *          seeded: false
+ */
+const ProviderSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    seeded: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  {
+    timestamps: true,
+  },
+);
 
 module.exports = mongoose.model('Provider', ProviderSchema);
