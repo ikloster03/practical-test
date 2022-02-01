e30fe136e0e82d1f03af2290828a2d17c486ff90
Ivan Monastyrev <ikloster@yandex.ru>
2022-02-01 23:15:52 +0300

    client(ui): add icon

---
+++ client/src/components/ui/UiIcon.vue
@@ -0,0 +1,55 @@
+<template>
+  <svg
+    aria-hidden="true"
+    :width="`${width}px`"
+    :height="`${height}px`"
+    :viewBox="`0 0 ${width} ${height}`">
+    <use
+      :href="symbolId"
+      :fill="color"
+      :stroke="strokeColor" />
+  </svg>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  name: 'UiIcon',
+  props: {
+    prefix: {
+      type: String,
+      default: 'icon',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: '#333',
+    },
+    strokeColor: {
+      type: String,
+      default: '',
+    },
+    width: {
+      type: Number,
+      default: 24,
+    },
+    height: {
+      type: Number,
+      default: 24,
+    },
+  },
+  setup(props) {
+    const symbolId = computed(() => `#${props.prefix}-${props.name}`);
+
+    return { symbolId };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
