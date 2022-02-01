76b527ca02da9d7bc3f2b9fe7801868696331dc8
Ivan Monastyrev <ikloster@yandex.ru>
2022-02-01 23:12:37 +0300

    client(vite config): add svg icon plugin + fix proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import path from 'path';
 
 export default ({ mode }) => {
@@ -9,7 +10,7 @@ export default ({ mode }) => {
     server: {
       port: process.env.VITE_APP_PORT || 8000,
       proxy: {
-        '/api': {
+        '/api/v1': {
           target: process.env.VITE_SERVER_URL || 'http://localhost:3000',
           changeOrigin: true,
           secure: false,
@@ -22,6 +23,12 @@ export default ({ mode }) => {
         '@': path.resolve(__dirname, 'src'),
       },
     },
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[name]',
+      }),
+    ],
   });
 };
