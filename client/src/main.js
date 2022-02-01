33cdeb4bd860d8c9fb97283915332bdc72223f7e
Ivan Monastyrev <ikloster@yandex.ru>
2022-02-01 23:18:49 +0300

    client(main): add svg icons register, modal, ui components, error handler

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,31 @@
 import { createApp } from 'vue';
+import VueUniversalModal from 'vue-universal-modal';
+import UiButton from '@/components/ui/UiButton.vue';
+import UiPreloader from '@/components/ui/UiPreloader.vue';
+import UiIcon from '@/components/ui/UiIcon.vue';
+import UiModal from '@/components/ui/UiModal.vue';
 import App from './App.vue';
+// eslint-disable-next-line import/no-unresolved
+import 'virtual:svg-icons-register';
+import 'vue-universal-modal/dist/index.css';
 import './index.css';
 
-createApp(App).mount('#app');
+const app = createApp(App)
+  .use(VueUniversalModal, {
+    teleportTarget: '#modals',
+    modalComponent: 'VueUniversalModal',
+  })
+  .component('ui-button', UiButton)
+  .component('ui-preloader', UiPreloader)
+  .component('ui-icon', UiIcon)
+  .component('ui-modal', UiModal);
+
+app.config.errorHandler = (err, vm, info) => {
+  // eslint-disable-next-line no-console
+  console.error(err);
+  // eslint-disable-next-line no-console
+  console.log(vm);
+  // eslint-disable-next-line no-console
+  console.info(info);
+};
+app.mount('#app');
