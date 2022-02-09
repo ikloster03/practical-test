6e6dd55b51b0d44f399f0aa1883a3aedd5dd96c8
Ivan Monastyrev <ikloster@yandex.ru>
2022-02-09 22:52:15 +0300

    client(client): add table

---
+++ client/src/components/client/ClientTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="client-table">
+    <div class="client-table__header">
+      <slot name="header" />
+    </div>
+    <div class="client-table__body">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ClientTable',
+};
+</script>
+
+<style scoped>
+.client-table__header {
+  @apply flex justify-between items-center p-5 bg-slate-100;
+}
+.client-table__body {
+ @apply relative divide-y divide-gray-200;
+}
+</style>
