9216913dd5db0ec96bcfe0297eafd147ae4400f7
Ivan Monastyrev <ikloster@yandex.ru>
2022-02-09 22:51:54 +0300

    client(ui): add group

---
+++ client/src/components/ui/UiGroup.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="ui-group">
+    <div class="ui-group__container">
+      <div class="ui-group__header">
+        <slot name="header">
+          {{ title }}
+        </slot>
+      </div>
+      <div class="ui-group__body">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiGroup',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style scoped>
+.ui-group {
+  @apply flex justify-center;
+}
+.ui-group__container {
+  @apply flex items-center w-3/4;
+}
+.ui-group__header {
+  @apply pr-3 w-2/12;
+}
+.ui-group__body {
+  @apply w-full w-10/12;
+}
+</style>
