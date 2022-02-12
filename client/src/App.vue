cc66810de1f6adcc57a550928e0fe6c81a8881ef
Ivan Monastyrev <ikloster@yandex.ru>
2022-02-12 14:51:00 +0300

    client(app->client): refactor - extract client table

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,77 +1,15 @@
 <template>
   <div class="container mx-auto py-10">
-    <ui-table>
-      <template #header>
-        <h1>
-          Clients
-        </h1>
-        <ui-button
-          class="flex"
-          @click.prevent="openModal(null)">
-          <ui-icon name="plus" />
-          <span>New Client</span>
-        </ui-button>
-      </template>
-      <template
-        v-if="!loading"
-        #default>
-        <ui-table-row
-          header
-          class="ui-table-row_client">
-          <ui-table-cell
-            v-for="header in HEADERS"
-            :key="`header-${header}`"
-            header>
-            {{ header }}
-          </ui-table-cell>
-        </ui-table-row>
-        <ui-table-row
-          v-for="client in clientList"
-          :key="`row-client-${client._id}`"
-          class="ui-table-row_client">
-          <ui-table-cell>
-            {{ client.name }}
-          </ui-table-cell>
-          <ui-table-cell>
-            {{ client.email }}
-          </ui-table-cell>
-          <ui-table-cell>
-            {{ formatPhoneNumber(client.phone) }}
-          </ui-table-cell>
-          <ui-table-cell chips>
-            <provider-chip
-              v-for="provider in client.providers"
-              :key="`provider-${client._id}-${provider._id}`"
-              :provider-name="provider.name" />
-          </ui-table-cell>
-          <ui-table-cell>
-            <ui-button
-              type="link"
-              @click.prevent="openModal(client)">
-              <span class="pr-2">
-                Edit
-              </span>
-              <ui-icon name="edit" />
-            </ui-button>
-          </ui-table-cell>
-          <ui-table-cell>
-            <ui-button
-              type="link"
-              @click.prevent="removeClient(client)">
-              <span class="pr-2">
-                Delete
-              </span>
-              <ui-icon name="trash" />
-            </ui-button>
-          </ui-table-cell>
-        </ui-table-row>
-      </template>
-      <ui-preloader v-if="loading" />
-    </ui-table>
+    <client-table
+      :loading="loading"
+      :clients="clientList"
+      @open-modal="openModal"
+      @remove-client="removeClient" />
     <client-modal-form
       v-model="showClientModalForm"
       v-bind="modal.attrs"
       v-on="modal.events" />
+    <ui-preloader v-if="loading" />
     <notifications position="bottom right" />
   </div>
 </template>
@@ -79,20 +17,14 @@
 <script>
 import { computed, reactive, ref } from 'vue';
 import { formatPhoneNumber } from '@/utils';
-import ProviderChip from '@/components/provider/ProviderChip.vue';
 import ClientModalForm from '@/components/client/ClientModalForm.vue';
 import useClient, { HEADERS } from '@/components/client/client';
 import useProvider from '@/components/provider/provider';
-import UiTable from '@/components/ui/table/UiTable.vue';
-import UiTableRow from '@/components/ui/table/UiTableRow.vue';
-import UiTableCell from '@/components/ui/table/UiTableCell.vue';
+import ClientTable from '@/components/client/ClientTable.vue';
 
 export default {
   components: {
-    UiTableCell,
-    UiTableRow,
-    UiTable,
-    ProviderChip,
+    ClientTable,
     ClientModalForm,
   },
   setup() {
@@ -133,7 +65,7 @@ export default {
       },
     });
 
-    const openModal = (client = null) => {
+    const openModal = (client) => {
       selectedClient.value = client;
       showClientModalForm.value = true;
     };
@@ -158,13 +90,5 @@ export default {
 </script>
 
 <style scoped>
-.ui-table-row_client {
-  grid-template-columns:
-    minmax(150px, 1fr)
-    minmax(250px, 1.5fr)
-    minmax(150px, 0.5fr)
-    minmax(350px, 1.5fr)
-    minmax(100px, 0.5fr)
-    minmax(100px, 0.5fr);
-}
+
 </style>
---
+++ client/src/components/client/ClientTable.vue
@@ -0,0 +1,126 @@
+<template>
+  <ui-table>
+    <template #header>
+      <h1>
+        Clients
+      </h1>
+      <ui-button
+        class="flex"
+        @click.prevent="openModal(null)">
+        <ui-icon name="plus" />
+        <span>New Client</span>
+      </ui-button>
+    </template>
+    <template
+      v-if="!loading"
+      #default>
+      <ui-table-row
+        header
+        class="ui-table-row_client">
+        <ui-table-cell
+          v-for="header in HEADERS"
+          :key="`header-${header}`"
+          header>
+          {{ header }}
+        </ui-table-cell>
+      </ui-table-row>
+      <ui-table-row
+        v-for="client in clients"
+        :key="`row-client-${client._id}`"
+        class="ui-table-row_client">
+        <ui-table-cell>
+          {{ client.name }}
+        </ui-table-cell>
+        <ui-table-cell>
+          {{ client.email }}
+        </ui-table-cell>
+        <ui-table-cell>
+          {{ formatPhoneNumber(client.phone) }}
+        </ui-table-cell>
+        <ui-table-cell chips>
+          <provider-chip
+            v-for="provider in client.providers"
+            :key="`provider-${client._id}-${provider._id}`"
+            :provider-name="provider.name" />
+        </ui-table-cell>
+        <ui-table-cell>
+          <ui-button
+            type="link"
+            @click.prevent="openModal(client)">
+            <span class="pr-2">
+              Edit
+            </span>
+            <ui-icon name="edit" />
+          </ui-button>
+        </ui-table-cell>
+        <ui-table-cell>
+          <ui-button
+            type="link"
+            @click.prevent="removeClient(client)">
+            <span class="pr-2">
+              Delete
+            </span>
+            <ui-icon name="trash" />
+          </ui-button>
+        </ui-table-cell>
+      </ui-table-row>
+    </template>
+  </ui-table>
+</template>
+
+<script>
+import UiTableCell from '@/components/ui/table/UiTableCell.vue';
+import UiTableRow from '@/components/ui/table/UiTableRow.vue';
+import UiTable from '@/components/ui/table/UiTable.vue';
+import ProviderChip from '@/components/provider/ProviderChip.vue';
+import { HEADERS } from '@/components/client/client';
+import { formatPhoneNumber } from '@/utils';
+
+export default {
+  name: 'ClientTable',
+  components: {
+    UiTableCell,
+    UiTableRow,
+    UiTable,
+    ProviderChip,
+  },
+  props: {
+    clients: {
+      type: Array,
+      default: () => [],
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['open-modal', 'remove-client'],
+  setup(props, { emit }) {
+    const openModal = (client) => {
+      emit('open-modal', client);
+    };
+    const removeClient = (client) => {
+      emit('remove-client', client);
+    };
+
+    return {
+      HEADERS,
+      formatPhoneNumber,
+      openModal,
+      removeClient,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.ui-table-row_client {
+  grid-template-columns:
+    minmax(150px, 1fr)
+    minmax(250px, 1.5fr)
+    minmax(150px, 0.5fr)
+    minmax(350px, 1.5fr)
+    minmax(100px, 0.5fr)
+    minmax(100px, 0.5fr);
+}
+</style>
